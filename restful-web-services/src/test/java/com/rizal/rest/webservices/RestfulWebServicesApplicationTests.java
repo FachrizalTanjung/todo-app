650d304471257e0cb7b5316941f0867ea1b51a2c
FachrizalTanjung <fachri_shsdua@yahoo.co.id>
2020-09-24 19:28:19 +0700

    add service

---
+++ restful-web-services/src/test/java/com/rizal/rest/webservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizal.rest.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import HelloWorldService from '../../api/todo/HelloWorldService.js'
 
 class WelcomeComponent extends Component {
     constructor(props) {
         super()
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.state = {
+            welcomeMessage: ''
+        }
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.handleError = this.handleError.bind(this)
     }
 
     render() {
@@ -19,12 +25,32 @@ class WelcomeComponent extends Component {
                     Click here to get customize welcome message.
                     <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
                 </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
             </>
         )
     }
 
     retrieveWelcomeMessage() {
-        console.log('retrieve clicked');
+        // HelloWorldService.executeHelloWorldService()
+        //     .then(response => this.handleSuccessfulResponse(response))
+
+        // HelloWorldService.executeHelloWorldBeanService()
+        //     .then(response => this.handleSuccessfulResponse(response))
+
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.match.params.name)
+            .then(response => this.handleSuccessfulResponse(response))
+            .catch(error => this.handleError(error))
+    }
+
+    handleSuccessfulResponse(response) {
+        this.setState({ welcomeMessage: response.data.message })
+    }
+
+    handleError(error) {
+        console.log(error.response.data.message)
+        this.setState({ welcomeMessage: error.response.data.message })
     }
 }
 
