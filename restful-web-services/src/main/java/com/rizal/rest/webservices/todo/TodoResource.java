c0775b6728d37cabc6b0ef13e753411c14a6f4d8
FachrizalTanjung <fachri_shsdua@yahoo.co.id>
2020-09-25 15:53:13 +0700

    todo

--- restful-web-services/src/main/java/com/rizal/rest/webservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/rizal/rest/webservices/todo/TodoResource.java
@@ -3,11 +3,15 @@ package com.rizal.rest.webservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoResource {
 
 	@Autowired
@@ -15,7 +19,17 @@ public class TodoResource {
 
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username) {
+//		Thread.sleep(10000);
 		return todoService.findAll();
+	}
+
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+		Todo todo = todoService.deleteById(id);
+		if (todo != null) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
